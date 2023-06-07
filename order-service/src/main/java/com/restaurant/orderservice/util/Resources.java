167f60144cbd1f712239375e1ffb5ad980e1dd48
Gergo Szalai <szerg97@gmail.com>
2023-06-07 13:44:38 +0200

    edited helm

--- order-service/src/main/java/com/restaurant/orderservice/util/Resources.java
+++ order-service/src/main/java/com/restaurant/orderservice/util/Resources.java
@@ -3,7 +3,6 @@ package com.restaurant.orderservice.util;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.restaurant.orderservice.model.MenuConfig;
-import org.springframework.core.io.ClassPathResource;
 
 import java.io.File;
 import java.io.IOException;
@@ -12,7 +11,7 @@ public class Resources {
 
     public static MenuConfig getMenuListResource(String url) throws IOException {
         ObjectMapper mapper = new ObjectMapper();
-        File file = new ClassPathResource(url).getFile();
+        File file = new File("/app/" + url);
         return mapper.readValue(file, new TypeReference<>() {
         });
     }
