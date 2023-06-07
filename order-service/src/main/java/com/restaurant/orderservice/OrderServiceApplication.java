565c6cc2caa08b05b6658e9cc3cb5309554ffd5b
Gergo Szalai <szerg97@gmail.com>
2023-06-07 15:17:40 +0200

    readded logging

--- order-service/src/main/java/com/restaurant/orderservice/OrderServiceApplication.java
+++ order-service/src/main/java/com/restaurant/orderservice/OrderServiceApplication.java
@@ -7,7 +7,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableFeignClients
-//@EnableScheduling
+@EnableScheduling
 public class OrderServiceApplication {
 
     public static void main(String[] args) {
